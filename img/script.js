81a5c4ad6205d74eeb6eb5daf7d721e978104a1f
Gabrielgomescorrea12 <gabriel.gomes.correa@escola.pr.gov.br>
2024-12-02 11:42:28 -0300

    gabR46NNYRBY

---
+++ img/script.js
@@ -0,0 +1,8 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click, function'(){
+        const atual = ducument.queryselector('.ativo');
+        const proximoPasso = 'paso-' + this.getAttribute('data-proximo');
+        atual.classList.remove('ativo');
+        ducument.getElementByid(proximoPasso).classList.add('ativo');
+        
